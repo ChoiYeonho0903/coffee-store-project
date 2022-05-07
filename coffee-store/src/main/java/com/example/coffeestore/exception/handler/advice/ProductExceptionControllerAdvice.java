4b0053d93bc6c67ba69f89629365ff8e233f4cfe
ChoiYeonho0903 <903yh@naver.com>
2022-05-08 03:08:38 +0900

    Exception Handler 구현

---
+++ coffee-store/src/main/java/com/example/coffeestore/exception/handler/advice/ProductExceptionControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.example.coffeestore.exception.handler.advice;
+
+import com.example.coffeestore.exception.handler.ErrorResult;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ProductExceptionControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExceptionHandler(IllegalArgumentException e) {
+        log.error("IllegalException", e);
+        return new ErrorResult("BAD_REQUEST", e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exceptionHandler(Exception e) {
+        log.error("Exception", e);
+        return new ErrorResult("INTERNAL_SERVER_ERROR", "내부 오류");
+    }
+
+
+}
